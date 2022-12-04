7f125b0e8e3b666139b3a985ee1ed82fa34c4304
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2022-12-04 14:05:09 -0300

    Update Queue.java

--- src/withlist/Queue.java
+++ src/withlist/Queue.java
@@ -10,43 +10,43 @@ public class Queue {
 	private List<Object> queue = new ArrayList<>();
 	
 	public void add(Object object) {
-        queue.add(object);
+        	queue.add(object);
 	}
 	
 	public boolean isEmpty() {
-        return queue.size() == 0;
+        	return queue.size() == 0;
 	}
 	
 	public int size() {
-        return queue.size();
+        	return queue.size();
 	}
 	
 	public List<Object> print() {
-        return queue;
+       		return queue;
 	}
 	
 	public Object get(int index) {
-        try {
-            return queue.get(index);
-        } catch (IndexOutOfBoundsException e) {
-            return "The given index is out of the bounds.";
-        }
+		try {
+		    return queue.get(index);
+		} catch (IndexOutOfBoundsException e) {
+		    return "The given index is out of the bounds.";
+		}
 	}
 	
 	public void set(int index, Object object) {
-        try {
-            
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("The given index is out of the bounds.");
-        }
+		try {
+
+		} catch (IndexOutOfBoundsException e) {
+		    System.out.println("The given index is out of the bounds.");
+		}
 	}
 	
 	public void remove() {
-        if (!isEmpty()) {
-            queue.remove(queue.get(0));
-        } else {
-            System.out.println("The stack is empty.");
-        }
+		if (!isEmpty()) {
+		    queue.remove(queue.get(0));
+		} else {
+		    System.out.println("The stack is empty.");
+		}
 	}
     
 }
